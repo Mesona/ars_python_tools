c5e9c30c8a039f28c7a94ee9c65fc6949a602bb4
mesona <kylemesona@gmail.com>
2020-11-13 18:27:19 -0800

    Initialize NPC creator

---
+++ NPCCreator/gen_characteristics.py
@@ -0,0 +1,32 @@
+#!/bin/python
+
+CHARACTERISTIC_BLOCKS = [
+  [2, 2, 2, 2, 1, 1, -1, -3],
+  [3, 2, 2, 1, 1, 0, -1, -3],
+  [3, 3, 1, 1, 0, -1, -2, -2],
+  [3, 2, 2, 1, -1, -1, -1, -2],
+  [2, 2, 2, 2, 1, 0, -2, -2],
+  [3, 3, 0, 0, 0, -1, -1, -2],
+  [2, 2, 2, 1, 0, 0, 0, -2],
+  [3, 1, 1, 0, 0, 0, 0, -1],
+]
+
+CHARACTERISTICS = {
+  "Intelligence" : 0,
+  "Perception" : 0,
+  "Strength" : 0,
+  "Stamina" : 0,
+  "Presence" : 0,
+  "Communication" : 0,
+  "Dexterity" : 0,
+  "Quickness" : 0,
+}
+
+def generate_characteristics():
+  import random
+  these_characteristics = CHARACTERISTIC_BLOCKS[random.randint(0, len(CHARACTERISTIC_BLOCKS) - 1)]
+  random.shuffle(these_characteristics)
+  for key in CHARACTERISTICS.keys():
+    CHARACTERISTICS[key] = these_characteristics.pop()
+
+  return CHARACTERISTICS
---
+++ NPCCreator/statblocks.py
@@ -0,0 +1,9 @@
+#!/bin/python
+
+class CharacteristicBlocks:
+  physical_foucsed = [
+  ]
+  mental_focus = [
+  ]
+  balanced = [
+  ]
