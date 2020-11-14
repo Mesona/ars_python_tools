7c1d39c97edf484256f8f9adf2b76fd7e3c7b86d
mesona <kylemesona@gmail.com>
2020-11-14 15:48:07 -0800

    Add flask_test.py

---
+++ flask_test.py
@@ -0,0 +1,11 @@
+#!/bin/python
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/test/')
+def flask_test():
+    return "yo"
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
