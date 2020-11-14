c1aed8e058808098321afbf30ced9c5243b66749
mesona <kylemesona@gmail.com>
2020-11-13 22:07:00 -0800

    Separate ability variables into abilities.py

---
+++ NPCCreator/abilities.py
@@ -0,0 +1,96 @@
+#!/bin/python
+
+ACADEMIC_ABILITIES = [
+  "Artes Liberales",
+  "Civil and Canon Law",
+  "Church Lore",
+  "Common Law",
+  "Dead Language",
+  "Medicine",
+  "Philosophiae",
+  "Theology",
+]
+
+EARLY_LIFE_ABILITIES = [
+  "Athletics",
+  "Awareness",
+  "Brawl",
+  "Charm",
+  "Folk Ken",
+  "Guile",
+  "Native Language",
+  "Stealth",
+  "Survival",
+  "Swim",
+]
+
+LATE_LIFE_ABILITIES = [
+  "Animal Handling",
+  "Area Lore",
+  "Bargain",
+  "Carouse",
+  "Chirugy",
+  "Concentration",
+  "Craft",
+  "Etiquette",
+  "Hunt",
+  "Intrigue",
+  "Leadership",
+  "Legerdemain",
+  "Living Language",
+  "Music",
+  "Organization Lore",
+  "Profession",
+  "Ride",
+  "Teaching",
+]
+
+MARTIAL_ABILITIES = [
+  "Bows",
+  "Great Weapon",
+  "Single Weapon",
+  "Thrown Weapon",
+]
+
+PROFESSIONAL_ABILITIES = [
+  "Bargain",
+  "Chirurgy",
+  "Civil and Canon Law",
+  "Common Law",
+  "Craft",
+  "Leadership",
+  "Medicine",
+  "Music",
+  "Organization Lore",
+  "Philosophiae",
+  "Profession",
+  "Teaching",
+  "Theology",
+]
+
+SOCIAL_ABILITIES = [
+  "Carouse",
+  "Charm",
+  "Etiquette",
+  "Folk Ken",
+  "Guile",
+  "Intrigue",
+  "Living Language",
+]
+
+SURVIVAL_ABILITIES = [
+  "Animal Handling",
+  "Area Lore",
+  "Awareness",
+  "Brawl",
+  "Concentration",
+  "Hunt",
+  "Legerdermain",
+  "Ride",
+  "Stealth",
+  "Survival",
+  "Swim",
+]
+
+GENERAL_ABILITIES = EARLY_LIFE_ABILITIES + LATE_LIFE_ABILITIES
+ALL_ABILITIES = GENERAL_ABILITIES + MARTIAL_ABILITIES + ACADEMIC_ABILITIES
